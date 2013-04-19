fc4b4e2ff6205714170bf4e440237c86934d84e3
arkchar <yurilxc@gmail.com>
2013-04-19 17:32:30 +0800

    use eval-after-load to improve python-settings

--- .emacs.d/programming/python-settings.el
+++ .emacs.d/programming/python-settings.el
@@ -1,9 +1,6 @@
-(defun python-mode-settings()
-  "Python settings for writings script."
-  (interactive())
-  (setq python-indent 4)
-  (setq tab-width 4))
-
-(add-hook 'python-mode-hook 'python-mode-settings)
+(eval-after-load "python"
+  `(let ((map org-mode-map))
+     (setq python-indent 4)
+     (setq tab-width 4)))
 
 (provide 'python-settings)
