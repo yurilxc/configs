d146df453c14b5c6217665d9fc3a0ec7d6bea7a8
arkchar <yurilxc@gmail.com>
2013-04-20 11:47:25 +0800

    Bug: eval-after-load to set python's buffer value change to use hook

--- .emacs.d/text-mode-settings/python-settings.el
+++ .emacs.d/text-mode-settings/python-settings.el
@@ -1,6 +1,7 @@
-(eval-after-load "python"
-  `(let ((map python-mode-map))
-     (setq python-indent 4)
-     (setq tab-width 4)))
+(add-hook 'python-mode-map
+	  '(lambda ()
+	     (let ((map python-mode-map))
+	       (setq python-indent-offset 4)
+	       (setq tab-width 4))))
 
 (provide 'python-settings)
