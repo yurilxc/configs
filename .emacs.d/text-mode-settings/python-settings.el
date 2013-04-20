c9d823498e40ade6e65601c9903ebeb8da0829eb
arkchar <yurilxc@gmail.com>
2013-04-20 14:21:03 +0800

    fix bug: add-hook map -> hook

--- .emacs.d/text-mode-settings/python-settings.el
+++ .emacs.d/text-mode-settings/python-settings.el
@@ -1,7 +1,7 @@
-(add-hook 'python-mode-map
-	  '(lambda ()
-	     (let ((map python-mode-map))
-	       (setq python-indent-offset 4)
-	       (setq tab-width 4))))
+(add-hook 'python-mode-hook
+		  (lambda ()
+			(let ((map python-mode-map))
+			  (setq python-indent-offset 4)
+			  (setq tab-width 4))))
 
 (provide 'python-settings)
