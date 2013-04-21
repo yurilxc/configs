a5ea2fa6c91113f8a7e2333db8b3cb025c936ffa
arkchar <yurilxc@gmail.com>
2013-04-21 16:21:31 +0800

    add hs-minor-mode keybindings

--- .emacs.d/text-mode-settings/text-mode-settings.el
+++ .emacs.d/text-mode-settings/text-mode-settings.el
@@ -1,3 +1,15 @@
+;; hs-mode
+;; TODO non-terminal
+;; (global-set-key (kbd "C-?") 'hs-toggle-hiding)
+;; (global-set-key (kbd "C-=") 'hs-show-block)
+;; (global-set-key (kbd "C-+") 'hs-show-all)
+;; (global-set-key (kbd "C--") 'hs-hide-block)
+;; (global-set-key (kbd "C-_") 'hs-hide-all)))
+(global-set-key (kbd "ESC [ 5") 'hs-show-block)
+(global-set-key (kbd "ESC [ 6") 'hs-show-all)
+(global-set-key (kbd "ESC [ 3") 'hs-hide-block)
+(global-set-key (kbd "ESC [ 4") 'hs-hide-all)
+
 (require 'yasnippet)
 (setq yas/root-directory (list (concat emacs-dir "snippets/")))
 (setq yas/indent-line 'fixed)
