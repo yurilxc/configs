f0ac192bbd95d99c59e24db07f4868405a4ca340
arkchar <yurilxc@gmail.com>
2013-04-22 14:57:04 +0800

    add rope support for python pymacs, ropemacs, rope

--- .emacs.d/text-mode-settings/python-settings.el
+++ .emacs.d/text-mode-settings/python-settings.el
@@ -1,7 +1,11 @@
+(ac-ropemacs-initialize)
 (add-hook 'python-mode-hook
 		  (lambda ()
 			(let ((map python-mode-map))
 			  (setq python-indent-offset 4)
-			  (setq tab-width 4))))
+			  (setq tab-width 4)
+			  (add-to-list 'ac-sources 'ac-source-ropemacs))))
+
+(setq ropemacs-enable-autoimport t)
 
 (provide 'python-settings)
