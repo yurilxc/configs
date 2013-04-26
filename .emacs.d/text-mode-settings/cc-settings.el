20d518c24d70af50552724eead04ad10a0a17621
arkchar <yurilxc@gmail.com>
2013-04-26 23:56:37 +0800

    group c++-mode-settings into cc-mode-initialize and add-hook it in c/c++-mode seperately.

--- .emacs.d/text-mode-settings/cc-settings.el
+++ .emacs.d/text-mode-settings/cc-settings.el
@@ -1,23 +1,29 @@
 (require 'xcscope)
+(require 'cc-mode)
 
-(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
-(add-to-list 'auto-mode-alist '("\\.c\\'" . c++-mode))
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c-mode))
+(add-to-list 'auto-mode-alist '("\\.c\\'" . c-mode))
+(add-to-list 'auto-mode-alist '("\\.cc\\'" . c++-mode))
+(add-to-list 'auto-mode-alist '("\\.cpp\\'" . c++-mode))
 
-(add-hook
- 'c++-mode-hook
- '(lambda ()
-    (let ((map c++-mode-map))
-      (linum-mode 1)
-      ;; blanks instead of tabs
-      (setq indent-tabs-mode nil)
-      (setq tab-width 4)
-      (setq c-basic-offset 4)
-      (add-to-list 'ac-sources 'ac-source-semantic-raw)
-      (add-to-list 'ac-sources 'ac-source-semantic)
-      (setq ac-sources (remove 'ac-source-yasnippet ac-sources))
-      (add-to-list 'ac-sources 'ac-source-yasnippet)
-      (define-key map (kbd "<f7>") 'compile)
-      (define-key map (kbd "<f8>") 'next-error)
-      (define-key map (kbd "<S-f8>") 'previous-error))))
+(defun cc-mode-initialize (map)
+  (interactive ())
+  (linum-mode 1)
+  ;; blanks instead of tabs
+  (setq indent-tabs-mode nil)
+  (setq tab-width 4)
+  (setq c-basic-offset 4)
+  (add-to-list 'ac-sources 'ac-source-semantic-raw)
+  (add-to-list 'ac-sources 'ac-source-semantic)
+  (setq ac-sources (remove 'ac-source-yasnippet ac-sources))
+  (add-to-list 'ac-sources 'ac-source-yasnippet)
+  (define-key map (kbd "<f7>") 'compile)
+  (define-key map (kbd "<f8>") 'next-error)
+  (define-key map (kbd "<S-f8>") 'previous-error))
+
+(add-hook 'c-mode-hook
+	  (lambda () (cc-mode-initialize c-mode-map)))
+(add-hook 'c++-mode-hook
+	  (lambda () (cc-mode-initialize c++-mode-map)))
 
 (provide 'cc-settings)
