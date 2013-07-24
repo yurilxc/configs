f90b1a1ac9552ff51eb844b604e2ed12aa6e495a
yurilxc <yurilxc@gmail.com>
2013-07-24 09:46:44 +0800

    update xmodmap.py to accomadate new xmodmap msg

--- .config/wmStartupScripts/xmodmap.py
+++ .config/wmStartupScripts/xmodmap.py
@@ -10,5 +10,5 @@ while True:
     for line in os.popen('xmodmap'):
         line = r.split(line)
         if line[0] == 'lock':
-            if line[2] == '(0x42)':
+            if '(0x42)' in line[2]:
                 os.system('xmodmap ~/.config/wmStartupScripts/xmodmap_swap_capslock_control')
